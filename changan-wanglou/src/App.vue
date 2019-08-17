ee6487bc6e89e4727b3cd8f63e7da1908a65c4f6
Dliveaman <1916794877@qq.com>
2019-08-17 18:00:11 +0800

    add

---
+++ changan-wanglou/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+   <div id="app" class="p-4">
+      <h2>《长安十二时辰》  望楼传信（二进制版）</h2>
+      <h4 class="chars">
+        <span v-for="(item, index) in chars" :key="index">
+          {{item}} {{index}}
+        </span>
+        </h4>
+      <div class="pt-3 d-flex justify-content-center">
+        <div class="box"
+         v-for="(n,i) in lights"
+         :key="i"
+         :class="{active:n}"
+         @click="$set(lights,i,1-n)"
+         >
+          {{n}} 
+        </div>
+      </div>
+      <h3 class="pt-3">
+        {{num}} : {{char}}
+      </h3>
+   </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        chars:'张都尉 狼 入 西市 东市 平康坊 永宁坊 皇城'.split(' '),
+        lights:Array(4).fill(0)
+      }
+    },
+    computed: {
+      num(){
+        return parseInt(this.lights.join(''),2)
+      },
+     
+      char(){
+        return this.chars[this.num]
+      }
+    },
+  }
+</script>
+
+<style>
+  body{
+    background: #000;
+    color: #ecc;
+    font-family: "STKaiti";
+    text-align: center
+  }
+  .box{
+    width:4rem;
+    height: 4rem;
+    background: #333;
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.5rem;
+  }
+  .box.active{
+    background: #ecc;
+    color: #000;
+  }
+  .chars span{
+    display: inline-block;
+    width: 2rem;
+    text-align: center
+  }
+</style>
---
+++ changan-wanglou/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
